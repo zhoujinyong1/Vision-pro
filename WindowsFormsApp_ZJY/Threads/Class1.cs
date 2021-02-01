db13907c97f84d9d1c4d9fab4b863f1bea698b1e
zhoujinyong1 <51100742+zhoujinyong1@users.noreply.github.com>
2021-02-01 16:45:54 +0800

    Add files via upload

---
+++ WindowsFormsApp_ZJY/Threads/Class1.cs
@@ -0,0 +1,32 @@
+﻿using Cognex.VisionPro.QuickBuild;
+using Cognex.VisionPro.ToolBlock;
+using Cognex.VisionPro.ToolGroup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp_ZJY.Threads
+{
+    class Class1
+    {
+        private static int execl =1;
+        private static int img =1;
+        private static int file =1;
+        private static string password = "";
+        private static int selectorzjy = 0;    //0表示未加载，1表示ZJY,2表示Select
+        private static CogToolGroup cogToolGroup = null;
+        private static CogToolBlock cogToolBlock = null;
+        private static CogJobManager cogJobManager = null;
+
+        public static int Execl { get => execl; set => execl = value; }
+        public static int Img { get => img; set => img = value; }
+        public static int File { get => file; set => file = value; }
+        public static string Password { get => password; set => password = value; }
+        public static int Selectorzjy { get => selectorzjy; set => selectorzjy = value; }
+        public static CogToolGroup CogToolGroup { get => cogToolGroup; set => cogToolGroup = value; }
+        public static CogToolBlock CogToolBlock { get => cogToolBlock; set => cogToolBlock = value; }
+        public static CogJobManager CogJobManager { get => cogJobManager; set => cogJobManager = value; }
+    }
+}
---
+++ WindowsFormsApp_ZJY/Threads/ThreadMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_ZJY.Threads
+{
+    public delegate void text(string str); //定义一个用于更新UI的委托，需要传入一个字符串参数
+    public delegate void threadmethod();//定义一个用于传入线程所运行的方法的委托，不需要参数
+    class ThreadMain
+    {
+        Control control;//线程依赖对象，一般为窗体上下文
+        text mymethod;//线程更新UI的方法，可以通过参数让用户自定义
+        //第一个窗体上下文，直接传this即可，第二个更新UI的方法名，第三个线程执行的方法名
+        public void runthread(Control con,text menthod, threadmethod method)
+        {
+            control = con; 
+            mymethod = menthod;
+      
+            Thread thread = new Thread(new ThreadStart (method));
+            thread.IsBackground = true;
+            thread.Start();
+        }
+    }
+}
